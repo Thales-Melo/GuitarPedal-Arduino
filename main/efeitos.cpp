2cd8ad05624d32136740ee7260e29145739b3b4b
Thales Melo <85687910+Thales-Melo@users.noreply.github.com>
2023-12-07 15:00:42 -0300

    add octave

--- main/efeitos.cpp
+++ main/efeitos.cpp
@@ -1,10 +1,12 @@
 #include "efeitos.h"
 #include "delay.h"
 #include "distortion.h"
+#include "octave_daft_punk.h"
 
 Effect effects[NUM_EFFECTS] = {
   {setupDelay, processDelay},         // Índice 0 representa o delay
-  {setupDistortion, processDistortion} // Índice 1 representa a distorção
+  {setupDistortion, processDistortion}, // Índice 1 representa a distorção
+  {setupOctave, processOctave}        // Índice 2 representa o octave_daft_punk
 };
 
 void setupEffects() {
@@ -17,7 +19,7 @@ void setupEffects() {
 void switchEffect(int effect) {
   if (effect >= 0 && effect < NUM_EFFECTS) {
     destroyDelay();
-    // setupEffects(); // Reinicia todos os efeitos
+    //setupEffects(); // Reinicia todos os efeitos
     effects[effect].setupEffect(); // Configura o efeito selecionado
   }
 }
