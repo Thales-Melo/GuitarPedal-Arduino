9ddae9fb25076cca5f55483160d91446b8462d2d
Thales Melo <85687910+Thales-Melo@users.noreply.github.com>
2023-12-08 12:13:46 -0300

    Update efeitos.cpp

--- main/efeitos.cpp
+++ main/efeitos.cpp
@@ -2,18 +2,21 @@
 #include "delay.h"
 #include "distortion.h"
 #include "octave_daft_punk.h"
+#include "clean.h"
 
 Effect effects[NUM_EFFECTS] = {
   {setupDelay, processDelay},         // Índice 0 representa o delay
   {setupDistortion, processDistortion}, // Índice 1 representa a distorção
-  {setupOctave, processOctave}        // Índice 2 representa o octave_daft_punk
+  {setupOctave, processOctave},      // Índice 2 representa o octave_daft_punk
+  {setupClean, processClean}         // Indice 3 representa o clean
 };
 
 void setupEffects() {
   // Inicialização de efeitos
-  for (int i = 0; i < NUM_EFFECTS; ++i) {
-    effects[i].setupEffect();
-  }
+  // for (int i = 0; i < NUM_EFFECTS; ++i) {
+  //   effects[i].setupEffect();
+  // }
+  effects[1].setupEffect();
 }
 
 void switchEffect(int effect) {
